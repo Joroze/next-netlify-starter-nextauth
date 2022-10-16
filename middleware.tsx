047a73fd2ff583dc2ebcc67e1ea18d8b1a1ad97b
Jordan Rosenberg <1338914+Joroze@users.noreply.github.com>
2022-10-16 17:08:16 -0400

    Test middleware

--- middleware.tsx
+++
@@ -1,9 +0,0 @@
-// middleware.ts
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  console.log('hi from middleware')
-  // return NextResponse.next()
-}
